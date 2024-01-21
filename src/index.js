72b5f1649dfac94e4485e44af4677df472a2cef8
Christian <habinezac15@gmail.com>
2024-01-21 19:48:52 +0200

    ft: root component rendered

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+function App() {
+  return <h1>Hello React!</h1>;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
